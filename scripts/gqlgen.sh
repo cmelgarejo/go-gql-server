716c4351a95e4ef46019682e53c623e73a766929
Christian Melgarejo Bresanovich <cmelgarejo.dev@gmail.com>
2019-12-30 06:55:44 -0300

    fix(scripts): gqlgen moved echo downstream

--- scripts/gqlgen.sh
+++ scripts/gqlgen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-printf "\nRegenerating gqlgen files\n"
 # Optional, delete the resolver to regenerate, only if there are new queries
 # or mutations, if you are just changeing the input or type definition and
 # doesn't impact the resolvers definitions, no need to do it
@@ -10,8 +9,8 @@ while [[ "$#" -gt 0 ]]; do case $1 in
     file in 'internal/gql/resolvers/'\n"
     rm -f internal/gql/resolvers/generated/resolver.go
   ;;
-  *) echo "Unknown parameter passed: $1"; exit 1;;
+  *) echo "Unknown parameter provided: $1"; exit 1;;
 esac; shift; done
-
+printf "\nRegenerating gqlgen files\n"
 time go run -v github.com/99designs/gqlgen
 printf "\nDone.\n\n"
